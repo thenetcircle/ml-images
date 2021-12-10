a77972205b7c4b45e2d55a6bb8c767cbd70b4773
Oscar Eriksson <oscar@thenetcircle.com>
2021-12-10 09:42:19 +0800

    multiprocessing

--- multiclass-classification/preprocess.py
+++ multiclass-classification/preprocess.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import time
 from glob import glob
 import argparse
-
+import pathlib
 import cv2
 from tqdm import tqdm
+from multiprocessing import Pool
 
 
 def parse_arguments():
@@ -16,46 +18,74 @@ def parse_arguments():
 
     _args = parser.parse_args()
 
-    if not os.path.exists(_args.input_dir):
-        print(f"error: input dir doesn't exist: {_args.input_dir}")
+    try:
+        n_workers = int(float(_args.workers))
+    except TypeError:
+        print(f"error: worker argument is not a valid int: {_args.workers}")
         sys.exit(1)
 
-    if not os.path.exists(_args.output_dir):
+    if n_workers < 1 or n_workers > 128:
+        print(f"error: worker amount needs to be in range [1, 128] but was '{n_workers}'")
+        sys.exit(1)
+
+    if not os.path.exists(_args.input):
+        print(f"error: input dir doesn't exist: {_args.input}")
+        sys.exit(1)
+
+    if not os.path.exists(_args.output):
         try:
-            os.mkdir(_args.output_dir)
+            pathlib.Path(_args.output).mkdir(parents=True, exist_ok=True)
         except Exception as e:
-            print(f"error: could not create output dir '{_args.output_dir}': {str(e)}")
+            print(f"error: could not create output dir '{_args.output}': {str(e)}")
             sys.exit(1)
 
     return _args
 
 
-def process_image(_file_path, _output_path):
-    temp_img = cv2.imread(_file_path, cv2.IMREAD_COLOR)
-    temp_img = cv2.cvtColor(temp_img, cv2.COLOR_RGB2BGR)
-    img_lab = cv2.cvtColor(temp_img, cv2.COLOR_BGR2Lab)
+def process_image(input_tuple):
+    try:
+        _file_path, _output_path = input_tuple
+        temp_img = cv2.imread(_file_path, cv2.IMREAD_COLOR)
+        temp_img = cv2.cvtColor(temp_img, cv2.COLOR_RGB2BGR)
+        img_lab = cv2.cvtColor(temp_img, cv2.COLOR_BGR2Lab)
 
-    l, a, b = cv2.split(img_lab)
-    img_l = clahe.apply(l)
-    img_clahe = cv2.merge((img_l, a, b))
+        l, a, b = cv2.split(img_lab)
+        img_l = clahe.apply(l)
+        img_clahe = cv2.merge((img_l, a, b))
 
-    img_clahe = cv2.cvtColor(img_clahe, cv2.COLOR_Lab2RGB)
-    cv2.imwrite(_output_path, img_clahe)
+        img_clahe = cv2.cvtColor(img_clahe, cv2.COLOR_Lab2RGB)
+        cv2.imwrite(_output_path, img_clahe)
+        sys.exit(1)
+    except Exception as e1:
+        print(f"error: could not process file '{file_path}' because: {str(e1)}")
 
 
 if __name__ == "__main__":
     args = parse_arguments()
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16, 16))
+    files = list()
 
-    for train_or_test in tqdm(glob(f"{args.input_dir}/*"), desc="train/test"):
-        for category_dir in tqdm(glob(f"{train_or_test}/*"), desc="category"):
-            for file_path in tqdm(glob(f"{category_dir}/*.jpg"), desc="images"):
-                output_path = f"{args.output_dir}/{file_path.split('/', maxsplit=1)[1]}"
+    for train_or_test in glob(f"{args.input}/*"):
+        for category_dir in glob(f"{train_or_test}/*"):
+            output_dir = f"{args.output}/{category_dir.split('/', maxsplit=1)[1]}"
+
+            if not os.path.exists(output_dir):
+                try:
+                    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+                except Exception as e:
+                    print(f"error: could not create output dir '{output_dir}': {str(e)}")
+                    sys.exit(1)
+
+            for file_path in glob(f"{category_dir}/*.jpg"):
+                output_path = f"{args.output}/{file_path.split('/', maxsplit=1)[1]}"
 
                 if os.path.exists(output_path) and not args.replace:
                     continue
 
-                try:
-                    process_image(file_path, output_path)
-                except Exception as e:
-                    print(f"error: could not process file '{file_path}' because: {str(e)}")
+                files.append((file_path, output_path))
+
+    print(len(files))
+
+    with Pool(processes=int(float(args.workers))) as pool:
+        results = tqdm(pool.imap(process_image, files), total=len(files))
+        tuple(results)  # fetch the lazy results
