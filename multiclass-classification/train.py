51f9a23bb2ad9b06b54db290c56d6de2574a69a1
Oscar Eriksson <oscar@thenetcircle.com>
2023-10-16 16:44:20 +0800

    start with larger LR, decrease faster; M instead of S, half BS

--- multiclass-classification/train.py
+++ multiclass-classification/train.py
@@ -138,12 +138,12 @@ plt.ioff()
 now = arrow.utcnow().format('YYMMDD_HHmm')
 
 # chooses the model size; larger is better, but requires a lot more memory and compute
-ENET_MODEL_VERSION = 'S'
+ENET_MODEL_VERSION = 'M'
 NUM_CLASSES = 3
-N_LAYERS_UNFREEZE = 10
-BATCH_SIZE = 256
+N_LAYERS_UNFREEZE = 20
+BATCH_SIZE = 128
 LEARNING_RATE_INITIAL = 1e-2
-LEARNING_RATE_TRANSFER = BATCH_SIZE / 32_000 / 8  # LR as in paper causes loss -> infinity
+LEARNING_RATE_TRANSFER = BATCH_SIZE / 32_000 / 2  # LR as in paper causes loss -> infinity
 EPOCHS_INITIAL = 5
 EPOCHS_TRANSFER = 150
 
@@ -200,7 +200,8 @@ early = EarlyStopping(
 reduce_lr = ReduceLROnPlateau(
     monitor='val_categorical_accuracy',
     factor=0.5,
-    patience=10,
+    patience=5,
+    min_lr=1e-6, # will stop reducing at around epoch 30 if halving every 5 epochs
     verbose=1
 )
 logging = TensorBoard(
